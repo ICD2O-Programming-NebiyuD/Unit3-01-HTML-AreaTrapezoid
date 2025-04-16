f0b9c5b0cc4e882e86ed5d33516bdbee867f54dc
Nebiyu Daniel <nebiyu.daniel@stu.ocsb.ca>
2025-04-16 14:06:43 +0000

    I calculated the area of a trapizode

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Ms Raffin All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Ms Raffin
+// Created on: March 2022
 // This file contains the JS functions for index.html
+
+"use strict";
+/**
+ * This function calculates area and perimeter of triangle.
+ */
+function calculate () {
+  // input length and width
+  let sideA = parseInt(document.getElementById('sideA-of-triangle').value);
+  let sideB = parseInt(document.getElementById('sideB-of-triangle').value);
+  let height = parseInt(document.getElementById('height-of-triangle').value);
+  // calculate area
+  // process
+  // let area = (sideA * sideB) / H2;
+  // process
+  let area = ((sideA + sideB) / 2)* height;
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+
+}
+
